388ad1994abb377884eb3782f1edde2d8117004a
RNehru <Roshdy.Nehru@gmail.com>
2024-09-10 12:49:35 +0300

    Small screens new design

--- lib/presentation/status_screens/default_success_screen.dart
+++ lib/presentation/status_screens/default_success_screen.dart
@@ -35,61 +35,123 @@ class DefaultSuccessScreen extends StatelessWidget {
           height: AppConstants.screenHeight(context),
           width: AppConstants.screenWidth(context),
           child: SafeArea(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: AppConstants.heightBasedOnFigmaDevice(context,
-                      AppConstants.screenHeight(context) < 600 ? 30 : 130),
-                ),
-                if (lottiePath != null) Lottie.asset(lottiePath!),
-                if (lottiePath == null)
-                  SvgPicture.asset(
-                    "assets/images/icons/success_img.svg",
+            child: AppConstants.screenHeight(context) < 600
+                ? ListView(
+                    physics: const BouncingScrollPhysics(),
+                    children: [
+                      SizedBox(
+                        height: AppConstants.heightBasedOnFigmaDevice(
+                            context,
+                            AppConstants.screenHeight(context) < 600
+                                ? 30
+                                : 130),
+                      ),
+                      if (lottiePath != null) Lottie.asset(lottiePath!),
+                      if (lottiePath == null)
+                        SvgPicture.asset(
+                          "assets/images/icons/success_img.svg",
+                        ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 18.0,
+                          bottom: 34.0,
+                          left: 20,
+                          right: 20,
+                        ),
+                        child: Text(
+                          title,
+                          style: AppFonts.ibm24HeaderBlue600,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Text(
+                          message,
+                          style: AppFonts.ibm15Grey400_600,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      DefaultButton(
+                        function: route == null
+                            ? onOkPressed
+                            : () {
+                                if (route != null) {
+                                  context.pushReplacementNamed(
+                                    route!,
+                                    arguments: arguments,
+                                  );
+                                }
+                              },
+                        text: "Ok",
+                        borderRadius: 0,
+                        border: Border.all(color: AppColors.buttonGreyBorder),
+                        marginLeft: 20,
+                        marginRight: 20,
+                        marginTop: 20,
+                        marginBottom:
+                            AppConstants.heightBasedOnFigmaDevice(context, 37),
+                      ),
+                    ],
+                  )
+                : Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: AppConstants.heightBasedOnFigmaDevice(
+                            context,
+                            AppConstants.screenHeight(context) < 600
+                                ? 30
+                                : 130),
+                      ),
+                      if (lottiePath != null) Lottie.asset(lottiePath!),
+                      if (lottiePath == null)
+                        SvgPicture.asset(
+                          "assets/images/icons/success_img.svg",
+                        ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 18.0,
+                          bottom: 34.0,
+                          left: 20,
+                          right: 20,
+                        ),
+                        child: Text(
+                          title,
+                          style: AppFonts.ibm24HeaderBlue600,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Text(
+                          message,
+                          style: AppFonts.ibm15Grey400_600,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      const Spacer(),
+                      DefaultButton(
+                        function: route == null
+                            ? onOkPressed
+                            : () {
+                                if (route != null) {
+                                  context.pushReplacementNamed(
+                                    route!,
+                                    arguments: arguments,
+                                  );
+                                }
+                              },
+                        text: "Ok",
+                        borderRadius: 0,
+                        border: Border.all(color: AppColors.buttonGreyBorder),
+                        marginLeft: 20,
+                        marginRight: 20,
+                        marginBottom:
+                            AppConstants.heightBasedOnFigmaDevice(context, 37),
+                      ),
+                    ],
                   ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    top: 18.0,
-                    bottom: 34.0,
-                    left: 20,
-                    right: 20,
-                  ),
-                  child: Text(
-                    title,
-                    style: AppFonts.ibm24HeaderBlue600,
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Text(
-                    message,
-                    style: AppFonts.ibm15Grey400_600,
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                const Spacer(),
-                DefaultButton(
-                  function: route == null
-                      ? onOkPressed
-                      : () {
-                          if (route != null) {
-                            context.pushReplacementNamed(
-                              route!,
-                              arguments: arguments,
-                            );
-                          }
-                        },
-                  text: "Ok",
-                  borderRadius: 0,
-                  border: Border.all(color: AppColors.buttonGreyBorder),
-                  marginLeft: 20,
-                  marginRight: 20,
-                  marginBottom:
-                      AppConstants.heightBasedOnFigmaDevice(context, 37),
-                ),
-              ],
-            ),
           ),
         ),
       ),
